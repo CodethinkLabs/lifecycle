a290ffa88998739dc870666b8b8324d0017f5b79
Bob Clough <bob.clough@codethink.co.uk>
2021-06-16 14:54:04 +0100

    fixes to lifecycle to pass linting

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,21 +1,24 @@
 """ Basic functionality tests """
 
 import pytest
-from lib.ConfigReader import ConfigReader
+from lib.config_reader import ConfigReader
 
 
 def test_config_folder():
+    """Test a folder full of working config files"""
     config = ConfigReader("tests/config/working", raw=True)
     assert config.config_raw["test"]["working"]
     assert config.config_raw["test2"]["working"]
 
 
 def test_config_file():
+    """Test a single working config file"""
     config = ConfigReader("tests/config/working/0.yml", raw=True)
     assert config.config_raw["test"]["working"]
 
 
 def test_config_broken():
+    """Test a single broken config file"""
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         ConfigReader("tests/config/broken.yml")
     assert pytest_wrapped_e.type == SystemExit
@@ -23,12 +26,14 @@ def test_config_broken():
 
 
 def test_config_environment_variable(monkeypatch):
+    """Test replacing an environment variable in a config file"""
     monkeypatch.setenv("TEST_VARIABLE", "banana")
     config = ConfigReader("tests/config/working/1.yml")
     assert config.config["test2"]["banana"] == "banana"
 
 
-def test_config_environment_variable_not_provided(monkeypatch):
+def test_config_environment_variable_not_provided():
+    """Test replacing an nonexistant environment variable in a config file"""
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         ConfigReader("tests/config/working/1.yml")
     assert pytest_wrapped_e.type == SystemExit
