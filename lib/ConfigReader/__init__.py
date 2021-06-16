221aee797c96330d3db35decf6ee1c10d4a4f663
Bob Clough <bob.clough@codethink.co.uk>
2021-06-16 12:16:31 +0100

    Add parsing for environment variables
    Adds the ability to add environment variables into the config.  
    
    This is a fairly naiive implementation using the string representation 
    of the dict as a string, but should work OK for our purposes.

--- lib/ConfigReader/__init__.py
+++ lib/ConfigReader/__init__.py
@@ -1,13 +1,17 @@
-# Config reading for lifecycle
+""" Config reading for lifecycle """
 
 import yaml
 import glob
+import os
 import os.path
+import string
+import collections
 
 
 class ConfigReader:
-    def __init__(self, file):
+    def __init__(self, file, raw=False):
         self.config = {}
+        self.config_raw = {}
 
         if os.path.isdir(file):
             filelist = sorted(glob.glob(file + "/*.yml"))
@@ -20,11 +24,29 @@ class ConfigReader:
         for file in filelist:
             with open(file, "r") as config_file:
                 try:
-                    self.config.update(yaml.safe_load(config_file))
+                    self.config_raw.update(yaml.safe_load(config_file))
                 except (yaml.YAMLError, ValueError) as exc:
                     print("Config read failed when parsing {}!".format(file))
                     print("Error was: {}".format(exc))
                     exit(1)
 
+        if not raw:
+            try:
+                self.config = eval(
+                    string.Template(str(self.config_raw)).substitute(**os.environ)
+                )
+            except KeyError as exc:
+                print(
+                    "The environment variable {} used in your config file wasn't provided!".format(
+                        exc
+                    )
+                )
+                exit(1)
+        else:
+            self.config = self.config_raw
+
     def print(self):
         print(yaml.dump(self.config, default_flow_style=False, default_style=""))
+
+    def print_raw(self):
+        print(yaml.dump(self.config_raw, default_flow_style=False, default_style=""))
--- tests/tests_config.py
+++ tests/tests_config.py
@@ -5,18 +5,31 @@ from lib.ConfigReader import ConfigReader
 
 
 def test_config_folder():
-    config = ConfigReader("tests/config/working")
-    assert config.config["test"]["working"]
-    assert config.config["test2"]["working"]
+    config = ConfigReader("tests/config/working", raw=True)
+    assert config.config_raw["test"]["working"]
+    assert config.config_raw["test2"]["working"]
 
 
 def test_config_file():
-    config = ConfigReader("tests/config/working/0.yml")
-    assert config.config["test"]["working"]
+    config = ConfigReader("tests/config/working/0.yml", raw=True)
+    assert config.config_raw["test"]["working"]
 
 
 def test_config_broken():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         config = ConfigReader("tests/config/broken.yml")
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
+
+
+def test_config_environment_variable(monkeypatch):
+    monkeypatch.setenv("TEST_VARIABLE", "banana")
+    config = ConfigReader("tests/config/working/1.yml")
+    assert config.config["test2"]["banana"] == "banana"
+
+
+def test_config_environment_variable_not_provided(monkeypatch):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        config = ConfigReader("tests/config/working/1.yml")
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
