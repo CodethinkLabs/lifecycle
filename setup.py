11a47093a24c315e86628d0bb3b28892348630cc
William Salmon <will.salmon@codethink.co.uk>
2021-06-21 18:02:37 +0100

    Add requirements to the package

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup, find_packages
 
+# Parse requirements files
+with open("requirements.txt") as f:
+    install_requirements = f.read().splitlines()
+
+with open("requirements-dev.txt") as f:
+    dev_requirements = f.read().splitlines()
+
+
 setup(
     name="lifecycle",
     packages=find_packages(exclude=("tests", "tests.*")),
+    install_requires=install_requirements,
+    extras_require={
+        "dev": dev_requirements,
+    },
     package_dir={"lifecycle": "lifecycle"},
     entry_points={
         "console_scripts": [
