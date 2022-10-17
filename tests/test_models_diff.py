b0aeef27a5e36b06044c761271b6870e2ada0ad9
Jonathan Maw <jonathan.maw@codethink.co.uk>
2022-10-17 18:38:30 +0100

    ModelDiff: Calculate the difference between two models
    ModelDiff is a class that will create separate dicts of all added, removed,
    changed and unchanged Users when passed a dict of Users from the source;
    a dict of Users from the target; and a config dict specifying which
    fields in the source are important, and regex patterns of all the groups
    from the source that are important.
    
    The "changed_users" property contains Users that have been merged from
    the two - any field specified in the config will be taken from the
    source, and any field not specified will be taken from the target.
    
    Similarly, it merges the list of groups by taking from the source
    any groups whose names match any of the regexes provided, and takes all
    groups that *don't* match any of the regexes from the target.

---
+++ tests/test_models_diff.py
@@ -0,0 +1,154 @@
+""" Tests for the model diffing engine"""
+
+import pytest
+
+from lifecycle.models import User, Group
+from lifecycle.model_diff import (
+    ModelDifference,
+    ModelDifferenceConfig,
+    InvalidField,
+    InvalidRegex,
+)
+
+# what to test:
+# bad config
+
+
+def test_basic_diff():
+    """Test a simple model diff"""
+    source_data = {
+        "test1": User("test1"),
+        "test2": User("test2"),
+    }
+    target_data = {
+        "test2": User("test2"),
+        "test3": User("test3"),
+    }
+    config = {
+        "fields": ["username"],
+    }
+
+    diff = ModelDifference.calculate(
+        source_data, target_data, ModelDifferenceConfig.from_dict(config)
+    )
+    assert "test1" in diff.added_users
+    assert "test3" in diff.removed_users
+    assert "test2" in diff.unchanged_users
+
+
+def test_diff_changed_user():
+    """Test diffs where a User has changed"""
+    source_data = {
+        "test1": User("test1"),
+        "test2": User("test2", groups=[Group("group1")]),
+    }
+    target_data = {
+        "test2": User("test2", groups=[Group("group2")]),
+        "test3": User("test3"),
+    }
+    config = {
+        "fields": [
+            "username",
+            "groups",
+        ],
+    }
+
+    diff = ModelDifference.calculate(
+        source_data, target_data, ModelDifferenceConfig.from_dict(config)
+    )
+    assert "test2" in diff.changed_users
+
+
+def test_diff_only_specified_fields():
+    """Test diffs where a User has changed, but not in a way the config cares about"""
+    source_data = {
+        "test1": User("test1"),
+        "test2": User("test2", groups=[Group("group1")]),
+    }
+    target_data = {
+        "test2": User("test2", groups=[Group("group2")]),
+        "test3": User("test3"),
+    }
+    config = {
+        "fields": [
+            "username",
+        ],
+    }
+
+    diff = ModelDifference.calculate(
+        source_data, target_data, ModelDifferenceConfig.from_dict(config)
+    )
+    assert "test2" in diff.unchanged_users
+
+
+def test_config_field_no_exist():
+    """Test that it catches config using fields that don't exist"""
+    source_data = {
+        "test1": User("test1"),
+        "test2": User("test2"),
+    }
+    target_data = {
+        "test2": User("test2"),
+        "test3": User("test3"),
+    }
+    config = {
+        "fields": ["foobarbaz"],
+    }
+
+    with pytest.raises(InvalidField):
+        ModelDifference.calculate(
+            source_data, target_data, ModelDifferenceConfig.from_dict(config)
+        )
+
+
+def test_config_bad_regex():
+    """Test that it catches config with malformed regular expressions"""
+    source_data = {
+        "test1": User("test1"),
+        "test2": User("test2"),
+    }
+    target_data = {
+        "test2": User("test2"),
+        "test3": User("test3"),
+    }
+    config = {
+        "fields": [
+            "username",
+            "groups",
+        ],
+        "groups_patterns": [
+            r"",
+            r"**",
+            r"\\",
+        ],
+    }
+
+    with pytest.raises(InvalidRegex):
+        ModelDifference.calculate(
+            source_data, target_data, ModelDifferenceConfig.from_dict(config)
+        )
+
+
+def test_groups_differ_by_pattern():
+    """Tests that differences in the 'groups' field obey groups_patterns"""
+    source_data = {
+        "test1": User("test1", groups=[Group("division1"), Group("project1")]),
+        "test2": User("test2", groups=[Group("division1"), Group("project1")]),
+    }
+    target_data = {
+        "test1": User("test1", groups=[Group("division2"), Group("project1")]),
+        "test2": User("test2", groups=[Group("division1"), Group("project2")]),
+    }
+    config = {
+        "fields": [
+            "username",
+            "groups",
+        ],
+        "groups_patterns": [r"^project\d+$"],
+    }
+
+    diff = ModelDifference.calculate(
+        source_data, target_data, ModelDifferenceConfig.from_dict(config)
+    )
+    assert "test1" in diff.unchanged_users
+    assert "test2" in diff.changed_users
